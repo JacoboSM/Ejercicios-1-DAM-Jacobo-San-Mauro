bcfdef9a700cade54f80414df32c4b5dfe86fb37
A716\Jacobo San Mauro <jacobitosmn@gmail.com>
2025-10-01 21:15:40 +0200

    Falta por hacer el ejercicio 8 de la bateria y acabar la practia 3

--- Ejercicios 1 DAM Jacobo San Mauro/src/main/java/org/example/Ejercicio_6_bateria_de_ejercicios_sencillos_Java_Programacion.java
+++ Ejercicios 1 DAM Jacobo San Mauro/src/main/java/org/example/Ejercicio_6_bateria_de_ejercicios_sencillos_Java_Programacion.java
@@ -9,40 +9,65 @@ public class Ejercicio_6_bateria_de_ejercicios_sencillos_Java_Programacion {
 
         Scanner read = new Scanner(System.in);
 
+        double h = 0;
         double b = 0;
-        boolean control = true
+        boolean control = true;
 
         do {
 
             try {
 
                 System.out.println("Dime la base del triangulo");
-                read.nextDouble();
+                b =read.nextDouble();
+
+                if (b <= 0){
+
+                    System.out.println("El valor de la base tiene que ser positivo");
+
+                } else {
+
+                    control = false;
+                }
 
             } catch (InputMismatchException e) {
 
-
+                System.out.println("Introduce solo numeros por favor");
+                read.nextLine();
 
             }
 
-        } while ()
+        }while (control);
 
+        control = true;
 
+        do {
 
-        if (b <= 0) {
-            System.out.println("El valor de la base tiene que ser positivo");
+            try {
 
-        } else {
-            System.out.println("Dime la altura del triangulo");
-            double h = read.nextDouble();
+                System.out.println("Dime la altura del triangulo");
+                h =read.nextDouble();
 
-            if (h <= 0) {
-                System.out.println("El valor de la altura tiene que ser positivo");
-            } else {
-                double area = (b * h) / 2;
-                System.out.println("El area del triangulo es " + area);
+                if (h <= 0){
+
+                    System.out.println("El valor de la altura tiene que ser positivo");
+
+                } else {
+
+                    control = false;
+                }
+
+            } catch (InputMismatchException e) {
+
+                System.out.println("Introduce solo numeros por favor");
+                read.nextLine();
+
             }
+
+            }while (control);
+
+        double area = (b * h) / 2;
+        System.out.println("El area del triangulo es " + area);
 
         }
     }
-}
+
--- Ejercicios 1 DAM Jacobo San Mauro/src/main/java/org/example/Ejercicio_7_bateria_de_ejercicios_sencillos_Java_Programacion.java
+++ Ejercicios 1 DAM Jacobo San Mauro/src/main/java/org/example/Ejercicio_7_bateria_de_ejercicios_sencillos_Java_Programacion.java
@@ -12,12 +12,21 @@ public class Ejercicio_7_bateria_de_ejercicios_sencillos_Java_Programacion {
 
         System.out.println("Vamos a hacer la lista de la compra");
 
-        System.out.println("¿ Tenemos tomates ?");
-        String tomates = read.next();
-        if (tomates == "si"){
+        System.out.println("¿ Tenemos tomates (S/N)?");
+        char tomates = read.next().charAt(0);
 
+        System.out.println("¿ Tenemos aceite (S/N)?");
+        char aceite = read.next().charAt(0);
+
+        System.out.println("¿ Tenemos jamon (S/N)?");
+        char jamon = read.next().charAt(0);
+
+        if (tomates == 'N' || aceite == 'N' || jamon == 'N'){
+
+            System.out.println("A comprar!!");
+
+        }else {
+            System.out.println("No comprar!!");
         }
     }
-
-
 }
