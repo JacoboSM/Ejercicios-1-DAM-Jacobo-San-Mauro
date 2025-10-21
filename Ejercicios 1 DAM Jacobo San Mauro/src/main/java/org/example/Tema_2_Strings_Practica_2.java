cda43f4ba63e3b63411b92383e1ab28ec2a0ddb9
A716\Jacobo San Mauro <jacobitosmn@gmail.com>
2025-10-21 17:17:40 +0200

    no message

--- Ejercicios 1 DAM Jacobo San Mauro/src/main/java/org/example/Tema_2_Strings_Practica_2.java
+++ Ejercicios 1 DAM Jacobo San Mauro/src/main/java/org/example/Tema_2_Strings_Practica_2.java
@@ -8,10 +8,13 @@ public class Tema_2_Strings_Practica_2 {
 
         Scanner read = new Scanner(System.in);
 
+         double num2 = 0;
+
         System.out.println("**** BENVENIDO A LA CALCULADORA RÁPIDA ****");
 
         System.out.println(">  Introduce el primer operando :");
         double num1 = read.nextDouble();
+        read.nextLine();
 
         System.out.println("----------------------------------");
         System.out.println("   [+] ->    sumar");
@@ -24,17 +27,42 @@ public class Tema_2_Strings_Practica_2 {
         System.out.println("Elige una operacíon :");
         String operacion = read.nextLine();
 
-        System.out.println(" > Introduce el segundo operando :");
-        double num2 = read.nextDouble();
+        if (operacion.toLowerCase().equals("r")){
 
-        switch (operacion){
+            System.out.println("Has elegido raíz cuadrada!");
+
+        }else {
+
+            System.out.println(" > Introduce el segundo operando :");
+            num2 = read.nextDouble();
+
+        }
+
+        switch (operacion.toLowerCase()){
 
             case "+":
+                System.out.println("Has elegido sumar!");
                 double resultado = num1 + num2;
                 System.out.println("El resultado de " + num1 + "+" + num2 + "es" + resultado);
 
             case "-":
+                System.out.println("Has elegido restar");
+                double resultado2 = num1 - num2;
+                System.out.println("El resultado de " + num1 + "-" + num2 + "es" + resultado2);
 
+            case "x":
+                System.out.println("Has elegido multiplicar!");
+                double resultado3 = num1 * num2;
+                System.out.println("El resultado de " + num1 + "*" + num2 + "es" + resultado3);
+
+            case "/":
+                System.out.println("Has elegido dividir!");
+                double resultado4 = num1 / num2;
+                System.out.println("El resultado de " + num1 + "/" + num2 + "es" + resultado4);
+
+            case "r":
+                double resultado5 = Math.sqrt(num1);
+                System.out.println("El resultado de la raiz cuadrada de  " + num1 + "es" + resultado5);
         }
 
 
