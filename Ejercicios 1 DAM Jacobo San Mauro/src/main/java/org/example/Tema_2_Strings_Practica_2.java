f9f0e9e13e5494d6e17ac2e5ac6b4343537c76ae
A716\Jacobo San Mauro <jacobitosmn@gmail.com>
2025-10-22 21:14:42 +0200

    no message

--- Ejercicios 1 DAM Jacobo San Mauro/src/main/java/org/example/Tema_2_Strings_Practica_2.java
+++ Ejercicios 1 DAM Jacobo San Mauro/src/main/java/org/example/Tema_2_Strings_Practica_2.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.nio.charset.MalformedInputException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Tema_2_Strings_Practica_2 {
@@ -9,12 +11,21 @@ public class Tema_2_Strings_Practica_2 {
         Scanner read = new Scanner(System.in);
 
          double num2 = 0;
+         double num1 = 0;
 
         System.out.println("**** BENVENIDO A LA CALCULADORA RÁPIDA ****");
 
-        System.out.println(">  Introduce el primer operando :");
-        double num1 = read.nextDouble();
-        read.nextLine();
+        try {
+
+            System.out.println(">  Introduce el primer operando :");
+            num1 = read.nextDouble();
+            read.nextLine();
+
+        } catch (InputMismatchException e) {
+
+            System.out.println("Utiliza solo numeros porfavor");
+            return;
+        }
 
         System.out.println("----------------------------------");
         System.out.println("   [+] ->    sumar");
@@ -33,39 +44,78 @@ public class Tema_2_Strings_Practica_2 {
 
         }else {
 
-            System.out.println(" > Introduce el segundo operando :");
-            num2 = read.nextDouble();
+            try{
+
+                System.out.println(" > Introduce el segundo operando :");
+                num2 = read.nextDouble();
+
+            }catch (InputMismatchException e){
+
+                System.out.println("Utiliza solo numeros porfavor");
+                return;
+
+            }
 
         }
 
         switch (operacion.toLowerCase()){
 
             case "+":
+
                 System.out.println("Has elegido sumar!");
                 double resultado = num1 + num2;
-                System.out.println("El resultado de " + num1 + "+" + num2 + "es" + resultado);
+                System.out.println("El resultado de " + num1 + " + " + num2 + " es " + resultado);
+                break;
 
             case "-":
+
                 System.out.println("Has elegido restar");
                 double resultado2 = num1 - num2;
-                System.out.println("El resultado de " + num1 + "-" + num2 + "es" + resultado2);
+                System.out.println("El resultado de " + num1 + " - " + num2 + " es " + resultado2);
+                break;
 
             case "x":
+
                 System.out.println("Has elegido multiplicar!");
                 double resultado3 = num1 * num2;
-                System.out.println("El resultado de " + num1 + "*" + num2 + "es" + resultado3);
+                System.out.println("El resultado de " + num1 + " * " + num2 + " es " + resultado3);
+                break;
 
             case "/":
-                System.out.println("Has elegido dividir!");
-                double resultado4 = num1 / num2;
-                System.out.println("El resultado de " + num1 + "/" + num2 + "es" + resultado4);
+
+                if (num2 == 0) {
+
+                    System.out.println("No se puede dividir entre 0");
+                    return;
+
+                }else {
+
+                    System.out.println("Has elegido dividir!");
+                    double resultado4 = num1 / num2;
+                    System.out.println("El resultado de " + num1 + " / " + num2 + " es " + resultado4);
+
+                }
+
+                break;
 
             case "r":
-                double resultado5 = Math.sqrt(num1);
-                System.out.println("El resultado de la raiz cuadrada de  " + num1 + "es" + resultado5);
+
+                if (num1 < 0){
+
+                    System.out.println("No se pueden hacer raizes negativas");
+                    return;
+
+                } else {
+                    double resultado5 = Math.sqrt(num1);
+                    System.out.println("El resultado de la raiz cuadrada de  " + num1 + " es "  + resultado5);
+
+                }
+                break;
+
+            default:
+
+                System.out.println("El signo no esta entre los mencionados prueba otra vez");
+
         }
-
-
     }
-
 }
