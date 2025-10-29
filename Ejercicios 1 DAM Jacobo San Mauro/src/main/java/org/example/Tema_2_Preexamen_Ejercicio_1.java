b666114a0f3f1797729f0c7ea201ddc5e3b3f35c
A716\Jacobo San Mauro <jacobitosmn@gmail.com>
2025-10-29 21:04:13 +0100

    no message

--- Ejercicios 1 DAM Jacobo San Mauro/src/main/java/org/example/Tema_2_Preexamen_Ejercicio_1.java
+++ Ejercicios 1 DAM Jacobo San Mauro/src/main/java/org/example/Tema_2_Preexamen_Ejercicio_1.java
@@ -4,7 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Tema_2_Random_Ejercicio_1 {
+public class Tema_2_Preexamen_Ejercicio_1 {
 
     public static void main (String[] args){
 
