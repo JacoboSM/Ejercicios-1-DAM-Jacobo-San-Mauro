c3c42f84fde7c0be6df9944a558906b149c52d7f
A716\Jacobo San Mauro <jacobitosmn@gmail.com>
2025-10-29 20:33:56 +0100

    no message

--- Ejercicios 1 DAM Jacobo San Mauro/src/main/java/org/example/Tema_2_Actividades_Random_Ejercicio_4.java
+++ Ejercicios 1 DAM Jacobo San Mauro/src/main/java/org/example/Tema_2_Actividades_Random_Ejercicio_4.java
@@ -3,23 +3,31 @@ package org.example;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Tema_2_Actividades_Random_Ejercicio_2 {
+public class Tema_2_Actividades_Random_Ejercicio_4 {
 
     public static void main (String[] args){
 
-        Scanner read = new Scanner (System.in);
+        Scanner read = new Scanner(System.in);
         Random random = new Random();
+
+        int min = 8;
+        int max = 12;
 
         String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
         String key = "";
 
-            for (int i=0;i<12;i++){
+        int num = random.nextInt(max - min +1 ) + min;
 
-                key += caracteres.charAt(random.nextInt(caracteres.length()-1));
+        for (int i=0;i<num;i++){
 
-            }
+            key += caracteres.charAt(random.nextInt(caracteres.length()-1));
+
+        }
 
         System.out.println(key);
+
+
     }
+
 }
