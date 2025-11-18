37427346e9c7c8a82ef09585342d3dfb3f4e3456
DESKTOP-ASG5CT4\JACOBO <jacobitosmn@gmail.com>
2025-11-18 14:43:37 +0100

    no message

--- Ejercicios 1 DAM Jacobo San Mauro/src/main/java/org/example/Tema_3_vectores_practica2.java
+++ Ejercicios 1 DAM Jacobo San Mauro/src/main/java/org/example/Tema_3_vectores_practica2.java
@@ -10,6 +10,8 @@ public class Tema_3_vectores_practica2 {
 
         Scanner read = new Scanner(System.in);
         Random random = new Random();
+        int max = 49
+        int min = 0;
 
         System.out.println("Bienvenido a la primitiva introduce los numeros de tu billete (recuerda que el formato tiene que ser N-N-N-N-N-N/R");
         String numeros_string = read.nextLine();
@@ -27,7 +29,46 @@ public class Tema_3_vectores_practica2 {
 
             }
 
-            int
+            int[] vector_numeros_suerte = new int [6]
+
+            for (int i = 0; i < vector_numeros_suerte.length ; i++){
+
+                int numero_random = random.nextInt(max - min + 1)-min;
+
+                vector_numeros_suerte[i] = numero_random
+
+
+            }
+
+            Array.sort(vector_numeros_suerte);
+
+            for (int i = 0; i <= vector.length-1; i++) {
+
+                if (vector[i] == vector[i+1]){
+
+                    vector[i] = -1000;
+
+                }else {
+
+                    tam++;
+
+                }
+
+            }
+
+            int[] limpio = new int [tam];
+            int j = 0;
+
+            for (int i = 0; i< vector.length; i++){
+
+                if (vector[i] != -1000){
+
+                    limpio[j] = vector[i];
+                    j++;
+
+                }
+
+            }
 
 
 
